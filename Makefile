d3da947362d031013da9eeb63b16f7ce796fd259
yssrexe <elyasiniyassir40@gmail.com>
2025-07-02 15:06:28 +0100

    create a temporary parsing

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ FLAGS = -Wall -Wextra -Werror
 SOURCE = main.c
 GET_SOURCE = get_next_line/get_next_line.c  get_next_line/get_next_line_utils.c
 LIBFT = LIBFT/ft_strlen.c LIBFT/ft_strncmp.c LIBFT/ft_strdup.c LIBFT/ft_split.c LIBFT/ft_atoi.c
-EXECUTABLE = Cub3D
+EXECUTABLE = cub3d
 OBJ = $(SOURCE:.c=.o)
 GET_OBJ = $(GET_SOURCE:.c=.o)
 LIBFT_OBJ = $(LIBFT:.c=.o)
--- main.c
+++ main.c
@@ -3,120 +3,98 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yael-yas <yael-yas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 05:12:21 by souel-bo          #+#    #+#             */
-/*   Updated: 2025/06/30 11:25:50 by souel-bo         ###   ########.fr       */
+/*   Updated: 2025/07/02 15:04:56 by yael-yas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub.h"
 
-bool	check_line(char *line)
+int is_map_line(char *str)
 {
-	if (!ft_strncmp(line, "NO", 2))
-		return (true);
-	else if (!ft_strncmp(line, "SO", 2))
-		return (true);
-	else if (!ft_strncmp(line, "WE", 2))
-		return (true);
-	else if (!ft_strncmp(line, "EA", 2))
-		return (true);
-	return (false);
-}
-
-t_textures *store_path(t_map *data, char *line, t_textures *textures)
-{
-	
-	if (!ft_strncmp(line, "NO", 2))
-		textures->north_path = ft_strdup(&line[3]);
-	else if (!ft_strncmp(line, "SO", 2))
-		textures->south_path = ft_strdup(&line[3]);
-	else if (!ft_strncmp(line, "WE", 2))
-		textures->west_path = ft_strdup(&line[3]);
-	else if (!ft_strncmp(line, "EA", 2))
-		textures->east_path = ft_strdup(&line[3]);
-	return textures;
-}
-
-bool check_collor(char *line)
-{
-	if (line[0] == 'F')
-		return true;
-	else if (line[0] == 'C')
-		return true;
-	return false;
-}
-
-t_map *parse_color(t_map *data, char *line)
-{
-	char **splited;
-	if (line[0] == 'F')
-	{
-		splited = ft_split(&line[2], ',');
-		data->floor_collors = malloc(sizeof(t_flor_collors));
-		data->floor_collors->red = ft_atoi(splited[0]);
-		data->floor_collors->green = ft_atoi(splited[1]);
-		data->floor_collors->blue = ft_atoi(splited[2]);
-	}
-	else if (line[0] == 'C')
-	{
-		splited = ft_split(&line[2], ',');
-		data->ceilling_collors = malloc(sizeof(t_ceilling_collors));
-		data->ceilling_collors->red = ft_atoi(splited[0]);
-		data->ceilling_collors->green = ft_atoi(splited[1]);
-		data->ceilling_collors->blue = ft_atoi(splited[2]);
-	}
-	return data;
-}
-
-t_map	*parse_map(char *map_file)
-{
-	int		fd;
-	t_map	*data;
-	char	*line;
 	int i = 0;
 
-	fd = open(map_file, O_RDONLY);
-	data = malloc(sizeof(t_map));
-	data->textures = malloc(sizeof(t_textures));
-	line = NULL;
+	if (!str || str[0] == '\n')
+		return 0;
+
+	while (str[i])
+	{
+		if (str[i] != '1' && str[i] != '0' && str[i] != ' ' &&
+			str[i] != '\n' && str[i] != 'N' && str[i] != 'S' &&
+			str[i] != 'E' && str[i] != 'W')
+			return 0;
+		i++;
+	}
+	return 1;
+}
+
+int count_lines(char **argv)
+{
+	int count = 0;
+	int fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return -1;
+
+	char *line;
+	int in_map = 0;
+
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		if (check_line(line))
-			data->textures = store_path(data, line, data->textures);
-		else if (check_collor(line))
-			data = parse_color(data, line);
-		else
-			i++;
+		if (is_map_line(line))
+		{
+			in_map = 1;
+			count++;
+		}
+		else if (in_map)
+		{
+			break;
+		}
 		free(line);
 	}
-	return (data);
+	close(fd);
+	return count;
 }
 
-void print(t_map *data)
-{
-	printf("%s", data->textures->north_path);
-	printf("%s", data->textures->south_path);
-	printf("%s", data->textures->west_path);
-	printf("%s", data->textures->east_path);
-	printf("%d ", data->floor_collors->red);
-	printf("%d ", data->floor_collors->green);
-	printf("%d \n", data->floor_collors->blue);
-	printf("%d ", data->ceilling_collors->red);
-	printf("%d ", data->ceilling_collors->green);
-	printf("%d \n", data->ceilling_collors->blue);
 
-}
-
-int	main(int ac, char **av)
+void start_parsing(t_map *units, char **argv)
 {
-	t_map *data;
-	if (ac != 2)
+	int fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return ;
+	
+	char *line ;
+	int i = 0;
+	int in_map = 0;
+	units->map = malloc(sizeof(char *) * (count_lines(argv) + 1));
+	while ((line = get_next_line(fd)) != NULL)
 	{
-		write(2, "Error\n", 6);
-		exit(1);
+		if (is_map_line(line))
+		{
+			units->map[i] = ft_strdup(line);
+			i++;
+			in_map = 1;
+		}
+		else if (in_map)
+		{
+			break;
+		}
+		free(line);
 	}
-	data = parse_map(av[1]);
-	print(data);
+	units->map[i] = NULL;
+	close(fd);
+}
+
+
+int main(int argc , char **argv)
+{
+	t_map units;
+	start_parsing(&units, argv);
+	int i = 0;
+	while (units.map[i])
+	{
+		printf("%s\n", units.map[i]);
+		i++;
+	}
 }
