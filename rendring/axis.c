a9d4dd8e39eea6176895847e93a823193d057987
yssrexe <elyasiniyassir40@gmail.com>
2025-07-27 16:35:11 +0100

    ff

--- rendring/axis.c
+++ rendring/axis.c
@@ -36,28 +36,30 @@ void	horizontal(t_mlx *all)
 	int	hit;
 	int	map_x;
 	int	map_y;
+	float py = all->map->player_y * 32;
+	float px = all->map->player_x * 32;
 
 	hit = 0;
 	float(first_horz_y), (first_horz_x), (Ya), (Xa);
-	if (-sin(all->map->angle) > 0)
+	if (sin(all->map->angle) > 0)
 	{
-		first_horz_y = floor(all->map->player_y) * TILE_SIZE - 1;
+		first_horz_y = floor(py * TILE_SIZE) / TILE_SIZE - 1;
 		Ya = -TILE_SIZE;
 	}
 	else
 	{
-		first_horz_y = floor(all->map->player_y) * TILE_SIZE + TILE_SIZE;
+		first_horz_y = floor(py / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
 		Ya = TILE_SIZE;
 	}
-	if (fabs(sin(all->map->angle)) < 0.0001f)
+	if (fabs(sin(all->map->angle)) < 0)
 	{
-		first_horz_x = all->map->player_x * TILE_SIZE;
+		first_horz_x = px;
 		Xa = (cos(all->map->angle) > 0) ? TILE_SIZE : -TILE_SIZE;
 	}
 	else
 	{
-		first_horz_x = (all->map->player_x * 32) + (first_horz_y
-				- (all->map->player_y * 32)) / tan(all->map->angle);
+		first_horz_x = px + (first_horz_y
+				- py) / tan(all->map->angle);
 		Xa = Ya / tan(all->map->angle);
 	}
 	while (!hit)
@@ -74,7 +76,7 @@ void	horizontal(t_mlx *all)
 			hit = 1;
 		mlx_pixel_put(all->connection, all->window, first_horz_x, first_horz_y,
 				GREEN);
-		printf("fhx : %f , fhy : %f\n", first_horz_x, first_horz_y);
+		//printf("fhx : %f , fhy : %f\n", first_horz_x, first_horz_y);
 		first_horz_x += Xa;
 		first_horz_y += Ya;
 	}
@@ -87,24 +89,27 @@ void	vertical(t_mlx *all)
 	int	map_y;
 
 	hit = 0;
+	float py = all->map->player_y * 32;
+	float px = all->map->player_x * 32;
+
 	float(first_vert_y), (first_vert_x), (Ya), (Xa);
 	if (cos(all->map->angle) > 0)
 	{
-		first_vert_x = floor((all->map->player_x * 32) / TILE_SIZE) * TILE_SIZE
+		first_vert_x = floor(px / TILE_SIZE) * TILE_SIZE
 					+ TILE_SIZE;
 		Xa = TILE_SIZE;
 	}
 	else
 	{
-		first_vert_x = floor((all->map->player_x * 32) / TILE_SIZE) * TILE_SIZE
+		first_vert_x = floor(px / TILE_SIZE) * TILE_SIZE
 					- 1;
 		Xa = -TILE_SIZE;
 	}
 	if (fabs(cos(all->map->angle)) < 0.0001f)
-		first_vert_y = all->map->player_y;
+		first_vert_y = py;
 	else
-		first_vert_y = (all->map->player_y * 32) + ((all->map->player_x * 32)
-			- first_vert_x) * tan(all->map->angle);
+		first_vert_y = py + (px
+		 - first_vert_x) * tan(all->map->angle);
 	Ya = Xa * tan(all->map->angle);
 	while (!hit)
 	{
@@ -130,6 +135,7 @@ void	vertical(t_mlx *all)
 
 void	dda_algorithm(t_mlx *all, float angle)
 {
+	printf("angle = %f\n", all->map->angle);
 	horizontal(all);
 	vertical(all);
 }
